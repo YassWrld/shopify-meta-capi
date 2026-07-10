f9f19a85442d06b7fbcf5a7fdf9331c3432a881b
Yass <yacineoubyi@gmail.com>
2026-07-10 15:45:00 +0100

    Multiple clients support

--- config/clients.js
+++ config/clients.js
@@ -1,13 +1,22 @@
 const clients = {
   [process.env.CLIENT_SLUG]: {
-    shopifySecret: process.env.SHOPIFY_WEBHOOK_SECRET,
-    metaPixelId:   process.env.META_PIXEL_ID,
-    metaCapiToken: process.env.META_CAPI_TOKEN,
-    testEventCode: process.env.META_TEST_EVENT_CODE || '',
-    storeUrl:      process.env.STORE_URL,
+    shopifySecret:      process.env.SHOPIFY_WEBHOOK_SECRET,
+    metaPixelId:        process.env.META_PIXEL_ID,
+    metaCapiToken:      process.env.META_CAPI_TOKEN,
+    testEventCode:      process.env.META_TEST_EVENT_CODE || '',
+    storeUrl:           process.env.STORE_URL,
     shopifyAdminToken:  process.env.SHOPIFY_ADMIN_TOKEN,
     shopifyStoreDomain: process.env.SHOPIFY_STORE_DOMAIN,
-  }
+  },
+  [process.env.CLIENT_TWO_SLUG]: {
+    shopifySecret:      process.env.CLIENT_TWO_SHOPIFY_WEBHOOK_SECRET,
+    metaPixelId:        process.env.CLIENT_TWO_META_PIXEL_ID,
+    metaCapiToken:      process.env.CLIENT_TWO_META_CAPI_TOKEN,
+    testEventCode:      process.env.CLIENT_TWO_META_TEST_EVENT_CODE || '',
+    storeUrl:           process.env.CLIENT_TWO_STORE_URL,
+    shopifyAdminToken:  process.env.CLIENT_TWO_SHOPIFY_ADMIN_TOKEN,
+    shopifyStoreDomain: process.env.CLIENT_TWO_SHOPIFY_STORE_DOMAIN,
+  },
 }
 
 module.exports = {
